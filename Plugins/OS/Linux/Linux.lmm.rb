f9c2d87058eb27b068b53c7dfb7fb7b3c11486db
Dāvis Mosāns <davispuh@gmail.com>
2024-07-15 19:55:58 +0300

    Linux: improve hosts file logic

--- Plugins/OS/Linux/Linux.lmm.rb
+++ Plugins/OS/Linux/Linux.lmm.rb
@@ -8,12 +8,12 @@ module ConfigLMM
 
             ISO_LOCATION = '~/.cache/configlmm/images/'
             HOSTS_FILE = '/etc/hosts'
-            HOSTS_SECTION_BEGIN = "# -----BEGIN CONFIGLMM-----\n"
-            HOSTS_SECTION_END   = "# -----END CONFIGLMM-----\n"
+            CONFIGLMM_SECTION_BEGIN = "# -----BEGIN CONFIGLMM-----\n"
+            CONFIGLMM_SECTION_END   = "# -----END CONFIGLMM-----\n"
             SUSE_NAME = 'openSUSE Leap'
 
             def actionLinuxBuild(id, target, activeState, context, options)
-                buildHostsFile(target, options)
+                buildHostsFile(id, target, options)
                 buildAutoYaST(id, target, options)
             end
 
@@ -34,7 +34,7 @@ module ConfigLMM
                 end
             end
 
-            def buildHostsFile(target, options)
+            def buildHostsFile(id, target, options)
                 if target['Hosts']
                     hosts  = "#\n"
                     hosts += "# /etc/hosts: static lookup table for host names\n"
@@ -42,14 +42,15 @@ module ConfigLMM
                     hosts += "#<ip-address>   <hostname.domain.org>   <hostname>\n"
                     hosts += "127.0.0.1       localhost\n"
                     hosts += "::1             localhost\n\n"
-                    hosts += HOSTS_SECTION_BEGIN
+                    hosts += CONFIGLMM_SECTION_BEGIN
                     target['Hosts'].each do |ip, entries|
                         hosts += ip.ljust(16) + entries.join(' ') + "\n"
                     end
-                    hosts += HOSTS_SECTION_END
+                    hosts += CONFIGLMM_SECTION_END
 
-                    mkdir(options['output'] + '/etc', options[:dry])
-                    fileWrite(options['output'] + HOSTS_FILE, hosts, options[:dry])
+                    path = options['output'] + '/' + id
+                    mkdir(path + '/etc', options[:dry])
+                    fileWrite(path + HOSTS_FILE, hosts, options[:dry])
                 end
             end
 
@@ -65,18 +66,18 @@ module ConfigLMM
             def deployLocalHostsFile(target, options)
                 if target['Hosts']
                     hostsLines = File.read(HOSTS_FILE).lines
-                    sectionBeginIndex = hostsLines.index(HOSTS_SECTION_BEGIN)
-                    sectionEndIndex = hostsLines.index(HOSTS_SECTION_END)
+                    sectionBeginIndex = hostsLines.index(CONFIGLMM_SECTION_BEGIN)
+                    sectionEndIndex = hostsLines.index(CONFIGLMM_SECTION_END)
                     if sectionBeginIndex.nil?
                         linesBefore = hostsLines
                         linesBefore << "\n"
-                        linesBefore << HOSTS_SECTION_BEGIN
-                        linesAfter = [HOSTS_SECTION_END]
+                        linesBefore << CONFIGLMM_SECTION_BEGIN
+                        linesAfter = [CONFIGLMM_SECTION_END]
                         linesAfter << "\n"
                     else
                         linesBefore = hostsLines[0..sectionBeginIndex]
                         if sectionEndIndex.nil?
-                            linesAfter = [HOSTS_SECTION_END]
+                            linesAfter = [CONFIGLMM_SECTION_END]
                             linesAfter << "\n"
                         else
                             linesAfter = hostsLines[sectionEndIndex..hostsLines.length]
