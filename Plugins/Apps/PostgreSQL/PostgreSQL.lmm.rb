04a15f9cd0905712a7a4790e4d73c4cd376496be
Dāvis Mosāns <davispuh@gmail.com>
2024-07-19 22:42:54 +0300

    Improve PostgreSQL

--- Plugins/Apps/PostgreSQL/PostgreSQL.lmm.rb
+++ Plugins/Apps/PostgreSQL/PostgreSQL.lmm.rb
@@ -16,31 +16,41 @@ module ConfigLMM
                 self.ensureServiceAutoStart(SERVICE_NAME, target['Location'])
 
                 if target['Location'] && target['Location'] != '@me'
+                    uri = Addressable::URI.parse(target['Location'])
+                    raise Framework::PluginProcessError.new("#{id}: Unknown Protocol: #{uri.scheme}!") if uri.scheme != 'ssh'
                     if target['ListenAll']
-                        uri = Addressable::URI.parse(target['Location'])
-                        raise Framework::PluginProcessError.new("#{id}: Unknown Protocol: #{uri.scheme}!") if uri.scheme != 'ssh'
-                        dir = ''
-                        self.class.sshStart(uri) do |ssh|
-                            distroID = self.class.distroIDfromSSH(ssh)
-                            dir = pgsqlDir(distroID)
-                            cmd = "sed -i 's|^host    all             all             127.0.0.1/32            ident|host    all             all             0.0.0.0/0               scram-sha-256|' #{dir + HBA_FILE}"
-                            self.class.sshExec!(ssh, cmd)
-                        end
+                        cmd = "sed -i 's|^host    all             all             127.0.0.1/32            ident|host    all             all             0.0.0.0/0               scram-sha-256|'"
+                        dir = updateConfigOverSSH(uri, cmd)
                         updateRemoteFile(uri, dir + CONFIG_FILE, options, false) do |configLines|
                             configLines << "listen_addresses = '*'\n"
                         end
+                    else
+                        cmd = "sed -i 's|^host    all             all             127.0.0.1/32            ident|host    all             all             127.0.0.1/32            scram-sha-256|'"
+                        updateConfigOverSSH(uri, cmd)
                     end
                 else
+                    dir = pgsqlDir(self.class.distroID)
                     if target['ListenAll']
-                        dir = pgsqlDir(self.class.distroID)
                         `sed -i 's|^host    all             all             127.0.0.1/32            ident|host    all             all             0.0.0.0/0            scram-sha-256|' #{dir + HBA_FILE}`
                         updateLocalFile(dir + CONFIG_FILE, options) do |configLines|
                             configLines << "listen_addresses = '*'"
                         end
+                    else
+                        `sed -i 's|^host    all             all             127.0.0.1/32            ident|host    all             all             127.0.0.1/32            scram-sha-256|' #{dir + HBA_FILE}`
                     end
                 end
 
                 self.startService(SERVICE_NAME, target['Location'])
+            end
+
+            def updateConfigOverSSH(uri, cmd)
+                dir = ''
+                self.class.sshStart(uri) do |ssh|
+                    distroID = self.class.distroIDfromSSH(ssh)
+                    dir = pgsqlDir(distroID)
+                    self.class.sshExec!(ssh, cmd + ' ' + dir + HBA_FILE)
+                end
+                dir
             end
 
             def self.createUserAndDBOverSSH(user, password, ssh)
