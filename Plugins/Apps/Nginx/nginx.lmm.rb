1a845ba05b6e0df0bdbe9dc7ba56f19118fc056b
Dāvis Mosāns <davispuh@gmail.com>
2024-07-22 20:40:55 +0300

    Refactor Nginx configs

--- Plugins/Apps/Nginx/nginx.lmm.rb
+++ Plugins/Apps/Nginx/nginx.lmm.rb
@@ -2,19 +2,19 @@
 module ConfigLMM
     module LMM
         class Nginx < Framework::NginxApp
-
-            CONFIG_DIR = '/etc/nginx/'
-            HTTP_DIR = '/srv/http/'
+            CERTBOT_PACKAGE = 'CertBotNginx'
 
             def actionNginxBuild(id, target, activeState, context, options)
+
                 dir = options['output'] + '/nginx/'
-                mkdir(dir, options[:dry])
+                mkdir(dir + 'conf.d', options[:dry])
+                mkdir(dir + 'servers-lmm', options[:dry])
                 copy(__dir__ + '/config-lmm', dir, options[:dry])
-                # TODO, maybe evaluate them as template?
                 copy(__dir__ + '/nginx.conf', dir, options[:dry])
-                copy(__dir__ + '/main.conf', dir, options[:dry])
-                mkdir(options['output'] + HTTP_DIR + 'root', options[:dry])
-                mkdir(options['output'] + HTTP_DIR + 'errors', options[:dry])
+                copy(__dir__ + '/conf.d/configlmm.conf', dir + 'conf.d/', options[:dry])
+
+                mkdir(options['output'] + WWW_DIR + 'root', options[:dry])
+                mkdir(options['output'] + WWW_DIR + 'errors', options[:dry])
             end
 
             # TODO
@@ -25,13 +25,56 @@ module ConfigLMM
             def actionNginxDeploy(id, target, activeState, context, options)
                 dir = options['output'] + '/nginx/'
 
-                if !target['Location'] || target['Location'] == '@me'
+                if target['Location'] && target['Location'] != '@me'
+                    uri = Addressable::URI.parse(target['Location'])
+                    raise Framework::PluginProcessError.new("Unknown Protocol: #{uri.scheme}!") if uri.scheme != 'ssh'
+                    self.class.sshStart(uri) do |ssh|
+                        Framework::LinuxApp.ensurePackageOverSSH(CERTBOT_PACKAGE, ssh)
+                        self.class.prepareNginxConfig(target, ssh)
+
+                        self.class.sshExec!(ssh, "mkdir -p #{CONFIG_DIR}conf.d")
+                        self.class.sshExec!(ssh, "mkdir -p #{WWW_DIR}root")
+                        self.class.sshExec!(ssh, "mkdir -p #{WWW_DIR}errors")
+                        ssh.scp.upload!(dir + 'nginx.conf', CONFIG_DIR + 'nginx.conf')
+                        ssh.scp.upload!(dir + 'conf.d/configlmm.conf', CONFIG_DIR + 'conf.d/configlmm.conf')
+                        self.class.uploadFolder(dir + 'config-lmm', CONFIG_DIR, ssh)
+                        self.class.uploadFolder(dir + 'servers-lmm', CONFIG_DIR, ssh)
+
+                        template = ERB.new(File.read(__dir__ + '/main.conf.erb'))
+                        renderTemplate(template, target, dir + 'main.conf', options)
+                        ssh.scp.upload!(dir + 'main.conf', CONFIG_DIR + 'main.conf')
+
+                        dir = "/etc/letsencrypt/live/Wildcard/"
+                        self.class.sshExec!(ssh, "mkdir -p #{dir}")
+                        # Need this temporarily so nginx can start
+                        if !self.class.remoteFilePresent?(dir + 'fullchain.pem', ssh)
+                            self.class.sshExec!(ssh, "openssl req -x509 -noenc -days 90 -newkey rsa:2048 -keyout #{dir}privkey.pem -out #{dir}fullchain.pem -subj '/C=US/O=ConfigLMM/CN=Wildcard'")
+                            self.class.sshExec!(ssh, "cp #{dir}fullchain.pem #{dir}chain.pem")
+                        end
+
+
+                    end
+                else
+                    self.class.prepareNginxConfig(target, nil)
+
                     copy(dir + '/config-lmm', CONFIG_DIR, options[:dry])
-                    copyNotPresent(dir + '/nginx.conf', CONFIG_DIR, options[:dry])
-                    copyNotPresent(dir + '/main.conf', CONFIG_DIR, options[:dry])
-                    copyNotPresent(dir + '/servers-lmm', CONFIG_DIR, options['dry'])
-                    mkdir(HTTP_DIR + 'root', options[:dry])
-                    mkdir(HTTP_DIR + 'errors', options[:dry])
+                    copy(dir + '/nginx.conf', CONFIG_DIR, options[:dry])
+
+                    copy(dir + '/servers-lmm', CONFIG_DIR, options['dry'])
+                    mkdir(WWW_DIR + 'root', options[:dry])
+                    mkdir(WWW_DIR + 'errors', options[:dry])
+
+                    template = ERB.new(File.read(__dir__ + '/main.conf.erb'))
+                    renderTemplate(template, target, dir + 'main.conf', options)
+                    copy(dir + '/main.conf', CONFIG_DIR, options[:dry])
+
+                    dir = "/etc/letsencrypt/live/Wildcard/"
+                    `mkdir -p #{dir}`
+                    if !File.exist?(dir + 'fullchain.pem')
+                        `openssl req -x509 -noenc -days 90 -newkey rsa:2048 -keyout #{dir}privkey.pem -out #{dir}fullchain.pem -subj "/C=US/O=ConfigLMM/CN=Wildcard"`
+                         `cp #{dir}fullchain.pem #{dir}chain.pem`
+                    end
+
                 end
                 # Consider:
                 # * Deploy on current host
--- lib/ConfigLMM/Framework/plugins/nginxApp.rb
+++ lib/ConfigLMM/Framework/plugins/nginxApp.rb
@@ -13,28 +13,45 @@ module ConfigLMM
 
         class NginxApp < Framework::Plugin
 
+            NGINX_PACKAGE = 'nginx'
+            CONFIG_DIR = '/etc/nginx/'
+            WWW_DIR = '/srv/www/'
+
             def writeNginxConfig(dir, name, id, target, activeState, context, options)
                 outputFolder = options['output']
 
                 updateTargetConfig(target)
 
+                target = target.dup
+                target['NginxVersion'] = 0 unless target['NginxVersion']
                 template = ERB.new(File.read(dir + '/' + name + '.conf.erb'))
                 renderTemplate(template, target, outputFolder + '/nginx/servers-lmm/' + name + '.conf', options)
-                plugins[:Nginx].actionNginxBuild(id, target, activeState, context, options)
             end
 
             def deployNginxConfig(id, target, activeState, context, options)
-                outputFolder = options['output']
+                outputFolder = options['output'] + '/nginx/servers-lmm'
 
-                if !target['Location'] || target['Location'] == '@me'
-                    copy(outputFolder + '/nginx/servers-lmm', '/etc/nginx/', options['dry'])
+                if target['Location'] && target['Location'] != '@me'
+                    uri = Addressable::URI.parse(target['Location'])
+                    raise Framework::PluginProcessError.new("Unknown Protocol: #{uri.scheme}!") if uri.scheme != 'ssh'
+                    self.class.sshStart(uri) do |ssh|
+                        self.class.uploadFolder(outputFolder, CONFIG_DIR, ssh)
+                    end
+                else
+                    copy(outputFolder, CONFIG_DIR, options['dry'])
                 end
-
-                plugins[:Nginx].actionNginxDeploy(id, target, activeState, context, options)
             end
 
             def cleanupNginxConfig(name, id, state, context, options)
                 rm('/etc/nginx/servers-lmm/' + name + '.conf', options['dry'])
+            end
+
+            def self.prepareNginxConfig(target, ssh)
+                if ssh
+                    target['NginxVersion'] = self.sshExec!(ssh, 'nginx -v').strip.split('/')[1].to_f
+                else
+                    target['NginxVersion'] = `nginx -v`.strip.split('/')[1].to_f
+                end
             end
 
             private
