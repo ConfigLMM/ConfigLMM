290c862f04ab68afe7bb37070e3899caccae97e8
Dāvis Mosāns <davispuh@gmail.com>
2024-09-14 21:30:33 +0300

    Improve MariaDB

--- Plugins/Apps/MariaDB/MariaDB.lmm.rb
+++ Plugins/Apps/MariaDB/MariaDB.lmm.rb
@@ -47,7 +47,9 @@ module ConfigLMM
 
             def self.createRemoteUserAndDB(settings, user, password, ssh = nil)
                 self.executeRemotely(settings, ssh) do |ssh|
-                    self.createUserAndDB(user, password, ssh)
+                    host = 'localhost'
+                    host = '%' if settings['HostName'] != 'localhost'
+                    self.createUserAndDB(user, password, host, ssh)
                 end
             end
 
@@ -62,11 +64,28 @@ module ConfigLMM
                 end
             end
 
-            def self.createUserAndDB(user, password, ssh = nil)
-                self.executeSQL("CREATE USER '#{user}'@'localhost'", nil, ssh, true)
-                self.executeSQL("ALTER USER '#{user}'@'localhost' IDENTIFIED BY '#{password}'", nil, ssh)
+            def self.createUserAndDB(user, password, host, ssh = nil)
+                self.executeSQL("CREATE USER '#{user}'@'#{host}'", nil, ssh, true)
+                self.executeSQL("ALTER USER '#{user}'@'#{host}' IDENTIFIED BY '#{password}'", nil, ssh)
                 self.executeSQL("CREATE DATABASE #{user}", nil, ssh, true)
-                self.executeSQL("GRANT ALL PRIVILEGES ON #{user}.* TO '#{user}'@'localhost'", nil, ssh)
+                self.executeSQL("GRANT ALL PRIVILEGES ON #{user}.* TO '#{user}'@'#{host}'", nil, ssh)
+            end
+
+            def self.createAdmin(ssh)
+                self.executeSQL("CREATE USER 'admin'@'%'", nil, ssh, true)
+                password = SecureRandom.alphanumeric(20)
+                self.executeSQL("ALTER USER 'admin'@'%' IDENTIFIED BY '#{password}'", nil, ssh)
+                self.executeSQL("GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION", nil, ssh)
+                password
+            end
+
+            def self.dropAdmin(ssh)
+                self.executeSQL("DROP USER 'admin'@'%'", nil, ssh, true)
+            end
+
+            def self.tableExist?(db, table, ssh)
+                table = self.executeSQL("SHOW TABLES LIKE '#{table}'", db, ssh).strip
+                !table.empty?
             end
 
             def self.executeSQL(sql, db = nil, ssh = nil, allowFailure = false)
