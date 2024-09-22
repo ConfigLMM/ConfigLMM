60c45b06814cd524216ccb3a449e677f5cfe81b7
Dāvis Mosāns <davispuh@gmail.com>
2024-09-22 21:55:55 +0300

    Nextcloud: Remove support for partial proxying

--- Plugins/Apps/Nextcloud/Nextcloud.lmm.rb
+++ Plugins/Apps/Nextcloud/Nextcloud.lmm.rb
@@ -20,57 +20,55 @@ module ConfigLMM
                     uri = Addressable::URI.parse(target['Location'])
                     raise Framework::PluginProcessError.new("#{id}: Unknown Protocol: #{uri.scheme}!") if uri.scheme != 'ssh'
                     self.class.sshStart(uri) do |ssh|
-                        if !target.key?('Proxy') || target['Proxy'] != 'only'
-                            Framework::LinuxApp.ensurePackages([PHP_FPM::PHPFPM_PACKAGE], ssh)
-                            Framework::LinuxApp.ensureServiceAutoStartOverSSH(PHP_FPM::PHPFPM_SERVICE, ssh)
-                            distroInfo = Framework::LinuxApp.ensurePackages([PACKAGE_NAME], ssh)
-                            addUserCmd = "#{distroInfo['CreateServiceUser']} --home-dir '#{HOME_DIR}' --create-home --comment 'Nextcloud' #{USER}"
-                            self.class.sshExec!(ssh, addUserCmd, true)
-                            self.class.sshExec!(ssh, "mkdir -p /var/log/php/ /var/lib/nextcloud/apps/ /var/lib/nextcloud/data/")
-                            self.class.sshExec!(ssh, "touch /var/log/php/nextcloud.errors.log")
-                            self.class.sshExec!(ssh, "touch /var/log/php/nextcloud.mail.log")
-                            self.class.sshExec!(ssh, "chown #{USER}:#{USER} /var/log/php/nextcloud.errors.log")
-                            self.class.sshExec!(ssh, "chown #{USER}:#{USER} /var/log/php/nextcloud.mail.log")
-                            PHP_FPM::fixConfigFileOverSSH(distroInfo, ssh)
+                        Framework::LinuxApp.ensurePackages([PHP_FPM::PHPFPM_PACKAGE], ssh)
+                        Framework::LinuxApp.ensureServiceAutoStartOverSSH(PHP_FPM::PHPFPM_SERVICE, ssh)
+                        distroInfo = Framework::LinuxApp.ensurePackages([PACKAGE_NAME], ssh)
+                        addUserCmd = "#{distroInfo['CreateServiceUser']} --home-dir '#{HOME_DIR}' --create-home --comment 'Nextcloud' #{USER}"
+                        self.class.sshExec!(ssh, addUserCmd, true)
+                        self.class.sshExec!(ssh, "mkdir -p /var/log/php/ /var/lib/nextcloud/apps/ /var/lib/nextcloud/data/")
+                        self.class.sshExec!(ssh, "touch /var/log/php/nextcloud.errors.log")
+                        self.class.sshExec!(ssh, "touch /var/log/php/nextcloud.mail.log")
+                        self.class.sshExec!(ssh, "chown #{USER}:#{USER} /var/log/php/nextcloud.errors.log")
+                        self.class.sshExec!(ssh, "chown #{USER}:#{USER} /var/log/php/nextcloud.mail.log")
+                        PHP_FPM::fixConfigFileOverSSH(distroInfo, ssh)
 
-                            webappsDir = PHP_FPM::webappsDir(distroInfo)
-                            configDir = webappsDir + 'nextcloud/config/'
-                            if !self.class.remoteFilePresent?(configDir + 'config.php', ssh)
-                                self.class.uploadNotPresent(__dir__ + '/config.php', configDir, ssh)
-                                self.class.sshExec!(ssh, "sed -i \"s|'instanceid' .*|'instanceid' => '#{SecureRandom.alphanumeric(10)}',|\" #{configDir}config.php")
-                                self.class.sshExec!(ssh, "touch #{configDir}CAN_INSTALL")
-                                self.class.sshExec!(ssh, "sed -i 's|/usr/share/webapps/|#{webappsDir}|' #{configDir}config.php")
-                            end
-                            self.class.sshExec!(ssh, "chown -R nextcloud:nextcloud #{configDir}")
-                            self.class.sshExec!(ssh, "chown -R nextcloud:nextcloud /var/lib/nextcloud/")
-
-                            target['Database'] ||= {}
-                            if !target['Database']['Type'] || target['Database']['Type'] == 'pgsql'
-                                PostgreSQL.createRemoteUserAndDBOverSSH(target['Database'], USER, nil, ssh)
-                            end
-
-                            target['User'] = USER unless target['User']
-                            name = 'nextcloud'
-                            self.updateRemoteFile(ssh, PHP_FPM.configDir(distroInfo) + name + '.conf', options, false, ';') do |configLines|
-                                PHP_FPM.writeConfig(name, target, distroInfo, configLines)
-                            end
-
-                            Framework::LinuxApp.startServiceOverSSH(PHP_FPM::PHPFPM_SERVICE, ssh)
+                        webappsDir = PHP_FPM::webappsDir(distroInfo)
+                        configDir = webappsDir + 'nextcloud/config/'
+                        if !self.class.remoteFilePresent?(configDir + 'config.php', ssh)
+                            self.class.uploadNotPresent(__dir__ + '/config.php', configDir, ssh)
+                            self.class.sshExec!(ssh, "sed -i \"s|'instanceid' .*|'instanceid' => '#{SecureRandom.alphanumeric(10)}',|\" #{configDir}config.php")
+                            self.class.sshExec!(ssh, "touch #{configDir}CAN_INSTALL")
+                            self.class.sshExec!(ssh, "sed -i 's|/usr/share/webapps/|#{webappsDir}|' #{configDir}config.php")
                         end
-                        if !target.key?('Proxy') || target['Proxy']
-                            self.class.prepareNginxConfig(target, ssh)
-                            self.writeNginxConfig(__dir__, 'Nextcloud', id, target, state, context, options)
-                            distroInfo = Framework::LinuxApp.ensurePackages([PACKAGE_NAME], ssh)
-                            webappsDir = PHP_FPM::webappsDir(distroInfo)
-                            nginxFile = options['output'] + '/nginx/servers-lmm/Nextcloud.conf'
-                            `sed -i 's|root .*|root #{webappsDir}nextcloud;|' #{nginxFile}`
-                            deployNginxConfig(id, target, activeState, context, options)
+                        self.class.sshExec!(ssh, "chown -R nextcloud:nextcloud #{configDir}")
+                        self.class.sshExec!(ssh, "chown -R nextcloud:nextcloud /var/lib/nextcloud/")
+
+                        target['Database'] ||= {}
+                        if !target['Database']['Type'] || target['Database']['Type'] == 'pgsql'
+                            PostgreSQL.createRemoteUserAndDBOverSSH(target['Database'], USER, nil, ssh)
                         end
+
+                        target['User'] = USER unless target['User']
+                        name = 'nextcloud'
+                        self.updateRemoteFile(ssh, PHP_FPM.configDir(distroInfo) + name + '.conf', options, false, ';') do |configLines|
+                            PHP_FPM.writeConfig(name, target, distroInfo, configLines)
+                        end
+
+                        Framework::LinuxApp.startServiceOverSSH(PHP_FPM::PHPFPM_SERVICE, ssh)
+
+                        self.class.ensurePackage(ssh)
+                        self.class.prepareNginxConfig(target, ssh)
+                        self.writeNginxConfig(__dir__, 'Nextcloud', id, target, state, context, options)
+                        distroInfo = Framework::LinuxApp.currentDistroInfo(ssh)
+                        webappsDir = PHP_FPM::webappsDir(distroInfo)
+                        nginxFile = options['output'] + '/nginx/servers-lmm/Nextcloud.conf'
+                        `sed -i 's|root .*|root #{webappsDir}nextcloud;|' #{nginxFile}`
+                        deployNginxConfig(id, target, activeState, context, options)
+                        Framework::LinuxApp.startService(NGINX_PACKAGE, ssh)
+                        self.class.reload(ssh)
                     end
                 else
-                    if !target.key?('Proxy') || target['Proxy']
-                        deployNginxConfig(id, target, activeState, context, options)
-                    end
+                    deployNginxConfig(id, target, activeState, context, options)
                     activeState['Location'] = '@me'
                 end
             end
