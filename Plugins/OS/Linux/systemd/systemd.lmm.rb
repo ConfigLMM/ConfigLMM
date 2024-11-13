2455473debc695e95cd9af36cfe04c170fb65ad4
Dāvis Mosāns <davispuh@gmail.com>
2024-11-13 15:43:27 +0200

    systemd: refactor

--- Plugins/OS/Linux/systemd/systemd.lmm.rb
+++ Plugins/OS/Linux/systemd/systemd.lmm.rb
@@ -7,20 +7,15 @@ module ConfigLMM
             USER_SERVICE_DIR = '/etc/systemd/system/user@.service.d/'
 
             def actionSystemdDeploy(id, target, activeState, context, options)
-                if target['Location'] && target['Location'] != '@me'
-                    if target['UserCgroups']
-                        uri = Addressable::URI.parse(target['Location'])
-                        raise Framework::PluginProcessError.new("#{id}: Unknown Protocol: #{uri.scheme}!") if uri.scheme != 'ssh'
-                        self.class.sshStart(uri) do |ssh|
-                            self.class.sshExec!(ssh, "mkdir -p #{USER_SERVICE_DIR}")
-                            ssh.scp.upload!(__dir__ + '/user-0.slice', SYSTEMD_CONFIG_PATH)
-                            ssh.scp.upload!(__dir__ + '/user@.service.d/delegate.conf', USER_SERVICE_DIR)
+                self.withConnection(target['Location'], target) do |connection|
+                    Linux.withConnection(connection) do |linuxConnection|
+                        if target['UserCgroups']
+                            linuxConnection.createDirs(options, USER_SERVICE_DIR)
+                            linuxConnection.upload(__dir__ + '/user-0.slice', SYSTEMD_CONFIG_PATH, options)
+                            linuxConnection.upload(__dir__ + '/user@.service.d/delegate.conf', USER_SERVICE_DIR, options)
                         end
                     end
-                else
-                    # TODO
                 end
-
             end
 
         end
