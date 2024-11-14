535daf56f35c6dd74b6317e32d2e64dc48a442e7
Dāvis Mosāns <davispuh@gmail.com>
2024-11-14 17:29:41 +0200

    GitLab: refactor

--- Plugins/Apps/GitLab/GitLab.lmm.rb
+++ Plugins/Apps/GitLab/GitLab.lmm.rb
@@ -1,44 +1,39 @@
 
 module ConfigLMM
     module LMM
-        class GitLab < Framework::NginxApp
+        class GitLab < Framework::Plugin
 
             HOME_DIR = '/var/lib/gitlab'
             IMAGE_ID = 'docker.io/gitlab/gitlab-ce:latest'
 
             def actionGitLabDeploy(id, target, activeState, context, options)
+                raise Framework::PluginProcessError.new('Domain field must be set!') unless target['Domain']
 
-                if target['Location'] && target['Location'] != '@me'
-                    uri = Addressable::URI.parse(target['Location'])
-                    raise Framework::PluginProcessError.new("#{id}: Unknown Protocol: #{uri.scheme}!") if uri.scheme != 'ssh'
+                self.withConnection(target['Location'], target) do |connection|
+                    Linux.withConnection(connection) do |linuxConnection|
 
-                    self.class.sshStart(uri) do |ssh|
-                        self.prepareConfig(target, ssh)
-
-                        distroInfo = Framework::LinuxApp.currentDistroInfo(ssh)
-                        self.class.exec("mkdir -p #{HOME_DIR}/config", ssh)
-                        self.class.exec("mkdir -p #{HOME_DIR}/logs", ssh)
-                        self.class.exec("mkdir -p #{HOME_DIR}/data", ssh)
-                        self.class.exec("mkdir -p #{HOME_DIR}/backups", ssh)
+                        linuxConnection.createDirs(options, "#{HOME_DIR}/config", "#{HOME_DIR}/logs", "#{HOME_DIR}/data", "#{HOME_DIR}/backups")
 
                         path = '/etc/containers/systemd'
-                        ssh.scp.upload!(__dir__ + '/GitLab.container', path)
+                        linuxConnection.upload(__dir__ + '/GitLab.container', path, options)
 
                         if !target.key?('Proxy') || target['Proxy']
-                            deployNginxProxyConfig('http://127.0.0.1:18100', 'GitLab', id, target, activeState, state, context, options, ssh)
+                            Nginx.withConnection(linuxConnection) do |nginxConnection|
+                                nginxConnection.provisionProxy('http://127.0.0.1:18100', 'GitLab', target, activeState, context, options)
+                            end
                         elsif target.key?('Proxy') && target['Proxy'] == false
-                            self.class.exec("sed -i 's|PublishPort=127.0.0.1:18100:|PublishPort=0.0.0.0:18100:|' #{path}/GitLab.container", ssh)
-                            Framework::LinuxApp.firewallAddPort('18100/tcp', ssh)
+                            linuxConnection.fileReplace("#{path}/GitLab.container", 'PublishPort=127.0.0.1:18100:', 'PublishPort=0.0.0.0:18100:', options)
+                            linuxConnection.firewallAddPort('18100/tcp', options)
                         end
 
-                        Framework::LinuxApp.reloadServiceManager(ssh)
-                        Framework::LinuxApp.restartService('GitLab', ssh)
+                        linuxConnection.reloadServiceManager(options)
+                        linuxConnection.restartService('GitLab', options)
 
                         configFile = HOME_DIR + '/config/gitlab.rb'
-                        while !self.class.remoteFilePresent?(configFile, ssh)
+                        while !linuxConnection.filePresent?(configFile, options)
                             sleep(2)
                         end
-                        updateRemoteFile(ssh, configFile, options, true) do |fileLines|
+                        linuxConnection.updateFile(configFile, options, true) do |fileLines|
                             fileLines << "external_url 'https://#{target['Domain']}'\n"
                             fileLines << "letsencrypt['enable'] = false\n"
                             fileLines << "nginx['listen_port'] = 80\n"
@@ -58,34 +53,29 @@ module ConfigLMM
                             end
                         end
 
-                        Framework::LinuxApp.restartService('GitLab', ssh)
+                        linuxConnection.restartService('GitLab', options)
                     end
-                else
-                    # TODO
                 end
-            end
-
-            def prepareConfig(target, ssh)
-              raise Framework::PluginProcessError.new('Domain field must be set!') unless target['Domain']
-
-              Framework::LinuxApp.ensurePackages([NGINX_PACKAGE], ssh)
-              self.class.prepareNginxConfig(target, ssh)
             end
 
             def cleanup(configs, state, context, options)
                 cleanupType(:GitLab, configs, state, context, options) do |item, id, state, context, options, connection|
-                    if item['Proxy'].nil? || item['Proxy']
-                        self.cleanupNginxConfig('GitLab', id, state, context, options, connection)
-                        self.class.reload(connection, options[:dry])
-                    end
-                    Framework::LinuxApp.firewallRemovePort('18100/tcp', connection, options[:dry])
-                    Framework::LinuxApp.stopService('GitLab', connection, options[:dry])
-                    connection.rm('/etc/containers/systemd/GitLab.container', options[:dry])
-                    connection.exec("podman rmi #{IMAGE_ID}", true, options[:dry])
-                    state.item(id)['Status'] = State::STATUS_DELETED unless options[:dry]
-                    if options[:destroy]
-                        connection.rm('/var/lib/gitlab', options[:dry])
-                        state.item(id)['Status'] = State::STATUS_DESTROYED unless options[:dry]
+                    Linux.withConnection(connection) do |linuxConnection|
+                        if item['Config']['Proxy'].nil? || item['Config']['Proxy']
+                            Nginx.withConnection(linuxConnection) do |nginxConnection|
+                                nginxConnection.cleanupConfig('GitLab', context, options)
+                                nginxConnection.reload(options)
+                            end
+                        end
+                        linuxConnection.firewallRemovePort('18100/tcp', options)
+                        linuxConnection.stopService('GitLab', options)
+                        linuxConnection.rm('/etc/containers/systemd/GitLab.container', options[:dry])
+                        linuxConnection.exec("podman rmi #{IMAGE_ID}", true, options)
+                        state.item(id)['Status'] = State::STATUS_DELETED unless options[:dry]
+                        if options[:destroy]
+                            connection.rm('/var/lib/gitlab', options[:dry])
+                            state.item(id)['Status'] = State::STATUS_DESTROYED unless options[:dry]
+                        end
                     end
                 end
             end
