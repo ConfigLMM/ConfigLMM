5e8ff5717dae53887daee13b603a86efe752d08d
Dāvis Mosāns <davispuh@gmail.com>
2024-11-17 16:04:58 +0200

    Cassandra: refactor

--- Plugins/Apps/Cassandra/Cassandra.lmm.rb
+++ Plugins/Apps/Cassandra/Cassandra.lmm.rb
@@ -6,36 +6,26 @@ module ConfigLMM
             SERVICE_NAME = 'cassandra'
 
             def actionCassandraDeploy(id, target, activeState, context, options)
-                plugins[:Linux].ensurePackage(PACKAGE_NAME, target['Location'])
-                plugins[:Linux].ensureServiceAutoStart(SERVICE_NAME, target['Location'])
+                self.withConnection(target['Location'], target) do |connection|
+                    Linux.withConnection(connection) do |linuxConnection|
+                        linuxConnection.ensurePackage(PACKAGE_NAME, options)
+                        linuxConnection.ensureServiceAutoStart(SERVICE_NAME, options)
 
-                if target['Location'] && target['Location'] != '@me'
-                    uri = Addressable::URI.parse(target['Location'])
-                    raise Framework::PluginProcessError.new("#{id}: Unknown Protocol: #{uri.scheme}!") if uri.scheme != 'ssh'
-
-                    self.class.sshStart(uri) do |ssh|
-                        distroInfo = Framework::LinuxApp.currentDistroInfo(ssh)
-                        configFile = '/etc/cassandra/cassandra.yaml'
-                        if distroInfo['Name'] == 'openSUSE Leap'
+                        if linuxConnection.distroInfo['Name'] == 'openSUSE Leap'
                             configFile = '/etc/cassandra/conf/cassandra.yaml'
                         end
 
-                        cmd = "sed -i 's|^uuid_sstable_identifiers_enabled:.*|uuid_sstable_identifiers_enabled: true|' #{configFile}"
-                        self.class.sshExec!(ssh, cmd)
+                        linuxConnection.fileReplace(configFile, '^uuid_sstable_identifiers_enabled:.*', 'uuid_sstable_identifiers_enabled: true', options)
                         if target['ClusterName']
-                            cmd = "sed -i 's|^cluster_name:.*|cluster_name: #{target['ClusterName']}|' #{configFile}"
-                            self.class.sshExec!(ssh, cmd)
+                            linuxConnection.fileReplace(configFile, '^cluster_name:.*', "cluster_name: #{target['ClusterName']}", options)
                         end
-                    end
-                else
-                    # TODO
-                end
 
-                plugins[:Linux].startService(SERVICE_NAME, target['Location'])
+                        linuxConnection.restartService(SERVICE_NAME, options)
+                    end
+                end
             end
 
         end
 
     end
 end
-
