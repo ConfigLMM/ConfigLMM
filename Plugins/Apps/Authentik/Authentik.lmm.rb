2215b06cdeb65ed26f1c008a0dfe997ca77a4285
Dāvis Mosāns <davispuh@gmail.com>
2024-11-17 16:22:35 +0200

    Authentik: refactor

--- Plugins/Apps/Authentik/Authentik.lmm.rb
+++ Plugins/Apps/Authentik/Authentik.lmm.rb
@@ -1,92 +1,79 @@
 
 module ConfigLMM
     module LMM
-        class Authentik < Framework::NginxApp
+        class Authentik < Framework::Plugin
 
             USER = 'authentik'
             HOME_DIR = '/var/lib/authentik'
             HOST_IP = '10.0.2.2'
 
             def actionAuthentikBuild(id, target, state, context, options)
-                self.writeNginxConfig(__dir__, 'Authentik', id, target, state, context, options)
+                Nginx.withConnection(local) do |nginxConnection|
+                    nginxConnection.writeConfig(__dir__, 'Authentik', target, state, context, options)
+                end
             end
 
             def actionAuthentikDeploy(id, target, activeState, context, options)
-                if target['Location'] && target['Location'] != '@me'
-                    uri = Addressable::URI.parse(target['Location'])
-                    case uri.scheme
-                    when 'ssh'
-                        self.class.sshStart(uri) do |ssh|
-                            self.prepareConfig(target, ssh)
+                raise Framework::PluginProcessError.new('Domain field must be set!') unless target['Domain']
 
-                            dbPassword = self.configurePostgreSQL(target['Database'], ssh)
-                            distroInfo = Framework::LinuxApp.currentDistroInfo(ssh)
-                            Framework::LinuxApp.configurePodmanServiceOverSSH(USER, HOME_DIR, 'Authentik IdP and SSO', distroInfo, ssh)
-                            self.class.sshExec!(ssh, "su --login #{USER} --shell /bin/sh --command 'mkdir -p ~/media'")
-                            self.class.sshExec!(ssh, "su --login #{USER} --shell /bin/sh --command 'mkdir -p ~/templates'")
-                            self.class.sshExec!(ssh, "su --login #{USER} --shell /bin/sh --command 'mkdir -p ~/certs'")
+                self.withConnection(target['Location'], target) do |connection|
+                    Linux.withConnection(connection) do |linuxConnection|
+                        target['Database'] ||= {}
 
-                            path = Framework::LinuxApp::SYSTEMD_CONTAINERS_PATH.gsub('~', HOME_DIR)
-                            self.class.sshExec!(ssh, " echo 'AUTHENTIK_SECRET_KEY=#{SecureRandom.urlsafe_base64(60)}' > #{path}/Authentik.env")
-                            self.class.sshExec!(ssh, " echo 'AUTHENTIK_REDIS__HOST=#{HOST_IP}' >> #{path}/Authentik.env")
-                            self.class.sshExec!(ssh, " echo 'AUTHENTIK_POSTGRESQL__HOST=#{HOST_IP}' >> #{path}/Authentik.env")
-                            self.class.sshExec!(ssh, " echo 'AUTHENTIK_POSTGRESQL__PASSWORD=#{dbPassword}' >> #{path}/Authentik.env")
-                            self.class.sshExec!(ssh, "chown #{USER}:#{USER} #{path}/Authentik.env")
-                            self.class.sshExec!(ssh, "chmod 600 #{path}/Authentik.env")
+                        dbPassword = self.configurePostgreSQL(target['Database'], linuxConnection, options)
 
-                            ssh.scp.upload!(__dir__ + '/Authentik-Server.container', path)
-                            ssh.scp.upload!(__dir__ + '/Authentik-Worker.container', path)
-                            self.class.sshExec!(ssh, "systemctl --user --machine=#{USER}@ daemon-reload")
-                            self.class.sshExec!(ssh, "systemctl --user --machine=#{USER}@ restart Authentik-Server")
-                            self.class.sshExec!(ssh, "systemctl --user --machine=#{USER}@ restart Authentik-Worker")
-
-                            Framework::LinuxApp.ensureServiceAutoStartOverSSH(NGINX_PACKAGE, ssh)
-                            self.writeNginxConfig(__dir__, 'Authentik', id, target, state, context, options)
-                            self.deployNginxConfig(id, target, activeState, context, options)
-                            Framework::LinuxApp.startServiceOverSSH(NGINX_PACKAGE, ssh)
-
-                            self.deployProxyOutpost(target, ssh)
+                        Podman.createUser(USER, HOME_DIR, 'Authentik IdP and SSO', linuxConnection, options)
+                        linuxConnection.withUserShell(USER) do |shell|
+                            shell.createDirs(options, '~/media', '~/templates', '~/certs')
                         end
-                    else
-                        raise Framework::PluginProcessError.new("#{id}: Unknown protocol: #{uri.scheme}!")
+
+                        path = Podman.containersPath(HOME_DIR)
+                        linuxConnection.fileWrite("#{path}/Authentik.env", "AUTHENTIK_SECRET_KEY=#{SecureRandom.urlsafe_base64(60)}", options)
+                        linuxConnection.fileAppend("#{path}/Authentik.env", "AUTHENTIK_REDIS__HOST=#{HOST_IP}", options)
+                        linuxConnection.fileAppend("#{path}/Authentik.env", "AUTHENTIK_POSTGRESQL__HOST=#{HOST_IP}", options)
+                        linuxConnection.fileAppend("#{path}/Authentik.env", "AUTHENTIK_POSTGRESQL__PASSWORD=#{dbPassword}", { **options, hide: true })
+
+                        linuxConnection.setUserGroup("#{path}/Authentik.env", USER, USER, options)
+                        linuxConnection.setPrivate("#{path}/Authentik.env", options)
+
+                        linuxConnection.upload(__dir__ + '/Authentik-Server.container', path, options)
+                        linuxConnection.upload(__dir__ + '/Authentik-Worker.container', path, options)
+
+                        linuxConnection.reloadUserServices(USER, options)
+                        linuxConnection.restartUserService(USER, 'Authentik-Server', options)
+                        linuxConnection.restartUserService(USER, 'Authentik-Worker', options)
+
+                        Nginx.withConnection(linuxConnection) do |nginxConnection|
+                            nginxConnection.provision(__dir__, 'Authentik', target, activeState, context, options)
+                        end
+
+                        self.deployProxyOutpost(target, linuxConnection, options)
                     end
-                else
-                    # TODO
                 end
             end
 
-            def deployProxyOutpost(target, ssh = nil)
+            def deployProxyOutpost(target, linuxConnection, options)
                 return unless target['Outposts'].to_a.include?('Proxy')
 
-                path = Framework::LinuxApp::SYSTEMD_CONTAINERS_PATH.gsub('~', HOME_DIR)
-                self.class.exec("echo 'AUTHENTIK_HOST=https://#{target['Domain'].downcase}' > #{path}/ProxyOutpost.env", ssh)
-                self.class.exec("echo 'AUTHENTIK_INSECURE=false' >> #{path}/ProxyOutpost.env", ssh)
-                self.class.exec(" echo 'AUTHENTIK_TOKEN=#{ENV['AUTHENTIK_TOKEN']}' >> #{path}/ProxyOutpost.env", ssh)
-                self.class.exec("chown #{USER}:#{USER} #{path}/ProxyOutpost.env", ssh)
-                self.class.exec("chmod 600 #{path}/ProxyOutpost.env", ssh)
+                path = Podman.containersPath(HOME_DIR)
+                linuxConnection.fileWrite("#{path}/ProxyOutpost.env", "AUTHENTIK_HOST=https://#{target['Domain'].downcase}", options)
+                linuxConnection.fileAppend("#{path}/ProxyOutpost.env", 'AUTHENTIK_INSECURE=false', options)
+                linuxConnection.fileAppend("#{path}/ProxyOutpost.env", "AUTHENTIK_TOKEN=#{ENV['AUTHENTIK_TOKEN']}", { **options, hide: true })
 
-                if ssh.nil?
-                    self.class.exec("cp #{__dir__ + '/Authentik-ProxyOutpost.container'} #{path}/")
-                else
-                    ssh.scp.upload!(__dir__ + '/Authentik-ProxyOutpost.container', path)
-                end
+                linuxConnection.setUserGroup("#{path}/ProxyOutpost.env", USER, USER, options)
+                linuxConnection.setPrivate("#{path}/ProxyOutpost.env", options)
 
-                self.class.exec("systemctl --user --machine=#{USER}@ daemon-reload", ssh)
-                self.class.exec("systemctl --user --machine=#{USER}@ restart Authentik-ProxyOutpost", ssh)
+                linuxConnection.upload(__dir__ + '/Authentik-ProxyOutpost.container', path)
+
+                linuxConnection.reloadUserServices(USER, options)
+                linuxConnection.restartUserService(USER, 'Authentik-ProxyOutpost', options)
             end
 
-            def prepareConfig(target, ssh)
-              target['Database'] ||= {}
-
-              raise Framework::PluginProcessError.new('Domain field must be set!') unless target['Domain']
-
-              Framework::LinuxApp.ensurePackages([NGINX_PACKAGE], ssh)
-              self.class.prepareNginxConfig(target, ssh)
-            end
-
-            def configurePostgreSQL(settings, ssh)
+            def configurePostgreSQL(dbSettings, linuxConnection, options)
                 password = SecureRandom.alphanumeric(20)
-                PostgreSQL.createRemoteUserAndDBOverSSH(settings, USER, password, ssh)
+                PostgreSQL.withConnection(dbSettings, linuxConnection) do |postgresConnection|
+                    postgresConnection.createUserAndDB(USER, password, options)
+                end
                 password
             end
 
