c53fec302523e6b8cce75b355e4b2438219d8412
Dāvis Mosāns <davispuh@gmail.com>
2024-11-19 15:44:17 +0200

    Implement LibreTranslate

---
+++ Plugins/Apps/LibreTranslate/LibreTranslate.lmm.rb
@@ -0,0 +1,33 @@
+
+module ConfigLMM
+    module LMM
+        class LibreTranslate < Framework::Plugin
+
+            USER = 'libretranslate'
+            HOME_DIR = '/var/lib/libretranslate'
+
+            def actionLibreTranslateDeploy(id, target, activeState, context, options)
+
+                self.withConnection(target['Location'], target) do |connection|
+                    Linux.withConnection(connection) do |linuxConnection|
+                        Podman.createUser(USER, HOME_DIR, 'LibreTranslate', linuxConnection, options)
+
+                        path = Podman.containersPath(HOME_DIR)
+
+                        linuxConnection.upload(__dir__ + '/LibreTranslate.container', path, options)
+
+                        args = ['--metrics']
+                        linuxConnection.fileReplace("#{path}/LibreTranslate.container", '$ARGS', args.join(' '), options)
+                        if target['Listen']
+                            linuxConnection.fileReplace("#{path}/LibreTranslate.container", '127.0.0.1', target['Listen'], options)
+                        end
+
+                        linuxConnection.reloadUserServices(USER, options)
+                        linuxConnection.restartUserService(USER, 'LibreTranslate', options)
+                    end
+                end
+            end
+
+        end
+    end
+end
