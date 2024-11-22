de71f0ad5d170dc3c8bb9e64f60686b332493721
Dāvis Mosāns <davispuh@gmail.com>
2024-11-22 18:25:12 +0200

    Network: support VLAN configuraion

--- Plugins/OS/Linux/Linux.lmm.rb
+++ Plugins/OS/Linux/Linux.lmm.rb
@@ -217,17 +217,20 @@ module ConfigLMM
                             linkType = target['Network']
                             target['Network'] = {}
                         end
+                        links.each do |link|
+                            target['Network']['Interfaces'][link] = 'manual' unless target['Network']['Interfaces'].key?(link)
+                        end
                         if !target['Network'].key?('Interfaces') ||
                            target['Network']['Interfaces'].to_h.empty? ||
                            !target['Network']['Interfaces'].key?(links.first)
                            target['Network']['Interfaces'] ||= {}
                            if !linkType.nil?
                                target['Network']['Interfaces'][links.first] = linkType
-                           else
+                           elsif target['Network']['IP']
                                target['Network']['Interfaces'][links.first] = {}
                                target['Network']['Interfaces'][links.first]['IP'] = target['Network']['IP']
-                               target['Network']['Interfaces'][links.first]['Gateway'] = target['Network']['Gateway']
-                               target['Network']['Interfaces'][links.first]['DNS'] = target['Network']['DNS']
+                               target['Network']['Interfaces'][links.first]['Gateway'] = target['Network']['Gateway'] if target['Network']['Gateway']
+                               target['Network']['Interfaces'][links.first]['DNS'] = target['Network']['DNS'] if target['Network']['DNS']
                            end
                         end
                         if target['Network']['Interfaces'].key?('vmbr0')
@@ -270,18 +273,30 @@ module ConfigLMM
                                     if data['IP']
                                         fileLines << "iface #{name} inet static\n"
                                         fileLines << "        address #{data['IP']}\n"
-                                        fileLines << "        gateway #{data['Gateway']}\n"
+                                        fileLines << "        gateway #{data['Gateway']}\n" if data['Gateway']
                                     else
                                         fileLines << "iface #{name} inet manual\n"
                                     end
-                                    if data['Ports']
-                                        fileLines << "        bridge-ports #{data['Ports'].join(' ')}\n"
+                                    if data.key?('Ports')
+                                        if data['Ports']
+                                            fileLines << "        bridge-ports #{data['Ports'].join(' ')}\n"
+                                        else
+                                            fileLines << "        bridge-ports none\n"
+                                        end
                                         fileLines << "        bridge-stp off\n"
                                         fileLines << "        bridge-fd 0\n"
+                                        if data['VLANFiltering']
+                                            fileLines << "        bridge-vlan-aware yes\n"
+                                        end
+                                        if data['VLANS']
+                                            fileLines << "        bridge-vids #{data['VLANS']}\n"
+                                        end
                                     end
-                                    fileLines << "        # dns-* options are implemented by the resolvconf package, if installed\n" if data['DNS']
-                                    fileLines << "        dns-nameservers #{data['DNS']}\n" if data['DNS']
-                                    fileLines << "        dns-search #{dnsSearch}\n" if dnsSearch
+                                    if data['DNS']
+                                        fileLines << "        # dns-* options are implemented by the resolvconf package, if installed\n"
+                                        fileLines << "        dns-nameservers #{data['DNS']}\n"
+                                        fileLines << "        dns-search #{dnsSearch}\n" if dnsSearch
+                                    end
                                 end
                                 fileLines << "\n"
                             end
