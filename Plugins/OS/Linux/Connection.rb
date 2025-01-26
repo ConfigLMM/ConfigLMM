394debfea7c3141c841580fff955425bab6da0ee
Dāvis Mosāns <davispuh@gmail.com>
2025-01-26 17:12:03 +0200

    Linux: Improve createServiceUser() error handling

--- Plugins/OS/Linux/Connection.rb
+++ Plugins/OS/Linux/Connection.rb
@@ -327,7 +327,8 @@ module ConfigLMM
 
             def createServiceUser(user, homedir, userComment = '', options = {})
                 addUserCmd = "#{distroInfo['CreateServiceUser']} --home-dir #{homedir.shellescape} --create-home --comment #{userComment.shellescape} #{user.shellescape}"
-                connection.exec(addUserCmd, true, options)
+                result = connection.exec(addUserCmd, true, options)
+                raise Framework::PluginProcessError.new(result) if result.strip.start_with?('useradd:') && !result.include?("user '#{user}' already exists")
                 connection.exec("chmod o-rwx #{homedir.shellescape}", false, options)
             end
 
