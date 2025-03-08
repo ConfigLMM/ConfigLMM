b19e33a7f5559b85383a7da9c3b53e59d838c073
Dāvis Mosāns <davispuh@gmail.com>
2025-03-08 20:54:29 +0200

    Filesystem: Support creating directories

--- Plugins/OS/General/Utils.lmm.rb
+++ Plugins/OS/General/Utils.lmm.rb
@@ -6,6 +6,12 @@ module ConfigLMM
             def actionFilesystemDeploy(id, target, activeState, context, options)
                 self.withConnection(target['Location'], target) do |connection|
                     Linux.withConnection(connection) do |linuxConnection|
+                        if target['Directory']
+                            linuxConnection.createDirs(options, target['Directory'])
+                        end
+                        if target['Directories']
+                            linuxConnection.createDirs(options, *target['Directories'])
+                        end
                         if target['Copy']
                             target['Copy'].each do |source, target|
                                 linuxConnection.upload(source, target, options)
