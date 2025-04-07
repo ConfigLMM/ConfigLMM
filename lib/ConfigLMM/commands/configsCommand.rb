1244fea910c1266a47317bff8f75e5d093fb229c
Dāvis Mosāns <davispuh@gmail.com>
2025-04-07 21:20:29 +0300

    Correctly update state Status when redeploying

--- lib/ConfigLMM/commands/configsCommand.rb
+++ lib/ConfigLMM/commands/configsCommand.rb
@@ -167,7 +167,7 @@ module ConfigLMM
                 activeState['Config'] = self.class.sanitizeConfig(singleTarget)
                 activeState['Config'].delete(:Parent)
                 activeState['Config'].delete('Resources')
-                activeState['Status'] = State::STATUS_DEPLOYED unless activeState['Status']
+                activeState['Status'] = State::STATUS_DEPLOYED if !activeState['Status'] || [State::STATUS_DELETED, State::STATUS_DESTROYED].include?(activeState['Status'])
                 state.save
             end
 
