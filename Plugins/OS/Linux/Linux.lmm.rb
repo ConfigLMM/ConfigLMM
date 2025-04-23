6309c0f8e067b8f9f7c5ec0d68b6e7eb344323f6
Dāvis Mosāns <davispuh@gmail.com>
2025-04-23 16:23:30 +0300

    Linux: Support configuring network for openSUSE installation

--- Plugins/OS/Linux/Linux.lmm.rb
+++ Plugins/OS/Linux/Linux.lmm.rb
@@ -758,9 +758,22 @@ module ConfigLMM
                 local.exec("sed -i 's|prompt		1|prompt		0|' #{cfg}")
                 local.exec("sed -i 's|timeout		600|timeout		1|' #{cfg}")
 
+                ifcfg = ''
+                if target['DefaultNetwork']['IP'] != 'dhcp'
+                    ifcfg = "ifcfg=\"eth*=#{target['DefaultNetwork']['IP']}"
+                    if target['DefaultNetwork']['Gateway'] || target['DefaultNetwork']['DNS']
+                        ifcfg +=  ',' + target['DefaultNetwork']['Gateway'].to_s
+                        if target['DefaultNetwork']['DNS']
+                            ifcfg +=  ',' + target['DefaultNetwork']['DNS']
+                            ifcfg +=  ',' + Addressable::IDNA.to_ascii(target['Domain']) if target['Domain']
+                        end
+                    end
+                    ifcfg += '"'
+                end
+
                 cfg = outputFolder + "EFI/BOOT/grub.cfg"
                 local.exec("sed -i 's|timeout=.*|timeout=1|' #{cfg}")
-                local.exec("sed -i 's|linux splash=silent|linux splash=silent autoyast=device://sr0/autoinst.xml|' #{cfg}")
+                local.exec("sed -i 's|linux splash=silent|linux splash=silent #{ifcfg} autoyast=device://sr0/autoinst.xml|' #{cfg}")
 
                 patchedIso = File.dirname(iso) + '/patched.iso'
                 local.exec("xorriso -as mkisofs -no-emul-boot -boot-info-table -boot-load-size 4 -iso-level 4 -b boot/x86_64/loader/isolinux.bin -c boot/x86_64/loader/boot.cat -eltorito-alt-boot -no-emul-boot -e boot/x86_64/efi -o #{patchedIso} #{outputFolder}")
